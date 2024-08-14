07cf945c993b089d820bc502c343eebe426f4d2a
Felipe Augusto <felipe.augusto@avanade.com>
2024-08-14 13:36:33 -0300

    feat: processing local xsd

---
+++ ApiManagementSchemaImport/Microsoft.Azure.ApiManagement.Common/ILog.cs
@@ -0,0 +1,57 @@
+namespace Microsoft.Azure.ApiManagement.Common
+{
+    public interface ILog
+    {
+        void Verbose(string eventName);
+
+        void Verbose(string eventName, string message);
+
+        void Verbose(string eventName, Exception ex);
+
+        void Verbose(string eventName, string message, Exception ex);
+
+        void Verbose(string eventName, string message, Exception ex, params object[] args);
+
+        void Informational(string eventName);
+
+        void Informational(string eventName, string message);
+
+        void Informational(string eventName, Exception ex);
+
+        void Informational(string eventName, string message, Exception ex);
+
+        void Informational(string eventName, string message, Exception ex, params object[] args);
+
+        void Warning(string eventName);
+
+        void Warning(string eventName, string message);
+
+        void Warning(string eventName, Exception ex);
+
+        void Warning(string eventName, string message, Exception ex);
+
+        void Warning(string eventName, string message, Exception ex, params object[] args);
+
+        void Error(string eventName);
+
+        void Error(string eventName, string message);
+
+        void Error(string eventName, Exception ex);
+
+        void Error(string eventName, string message, Exception ex);
+
+        void Error(string eventName, string message, Exception ex, params object[] args);
+
+        void Critical(string eventName);
+
+        void Critical(string eventName, string message);
+
+        void Critical(string eventName, Exception ex);
+
+        void Critical(string eventName, string message, Exception ex);
+
+        void Critical(string eventName, string message, Exception ex, params object[] args);
+
+        void Success(string msg);
+    }
+}
--- ApiManagementSchemaImport/Microsoft.Azure.ApiManagement.WsdlProcessor.App/Program.cs
+++ ApiManagementSchemaImport/Microsoft.Azure.ApiManagement.WsdlProcessor.App/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Azure.ApiManagement.WsdlProcessor.Common;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
@@ -13,164 +11,63 @@ namespace Microsoft.Azure.ApiManagement.WsdlProcessor.App
     {
         static async Task Main(string[] args)
         {
+            string wsdlFile, outputFile, pathSchemaReference = string.Empty;
+
             var log = new ConsoleLog();
-            string wsdlFile;
-            string outputFile;
-            if (args.Length == 2)
+            int exitCode = 0;
+
+            if (args.Length == 2 || args.Length == 3)
             {
                 wsdlFile = args[0];
+                if (!File.Exists(wsdlFile))
+                {
+                    var msg = $"The input file {wsdlFile} does not exists or we cannot access.";
+                    log.Error(msg);
+                    throw new Exception(msg);
+                }
+
                 wsdlFile = wsdlFile.Contains(".wsdl") ? wsdlFile : wsdlFile + ".wsdl";
+
                 outputFile = args[1];
                 outputFile = Path.IsPathRooted(outputFile) ? outputFile : Path.Join(Directory.GetCurrentDirectory(), outputFile);
+
+                pathSchemaReference = args.Length == 3 ? args[2] : string.Empty;
             }
             else
             {
                 Console.WriteLine("Please enter a wsdl file to process and output file.");
+                Environment.Exit(1);
+
                 return;
             }
+
             try
             {
                 var wsdlString = File.ReadAllText(wsdlFile);
                 var xDocument = XDocument.Parse(wsdlString);
-                await WsdlDocument.LoadAsync(xDocument.Root, log);
+                await WsdlDocument.LoadAsync(xDocument.Root, log, pathSchemaReference);
+
                 xDocument.Root.Save(outputFile);
-            }catch(XmlException e)
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                log.Success($"*** WSDL file {wsdlFile} processed successfully and generate file {outputFile}.");
+            }
+            catch (XmlException e)
             {
+                exitCode = 1;
                 log.Error($"{e.Message}");
                 log.Error($"Location {wsdlFile} contains invalid xml: {e.StackTrace}");
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
+                exitCode = 1;
                 log.Error($"{e.Message}");
                 log.Error($"Stacktrace: {e.StackTrace}");
             }
-        }
-    }
 
-    public class ConsoleLog : ILog
-    {
-        public void Critical(string eventName)
-        {
-            Console.WriteLine("Critical : " + eventName);
-        }
-
-        public void Critical(string eventName, string message)
-        {
-            Console.WriteLine("Critical : " + eventName + " : " + message);
-        }
-
-        public void Critical(string eventName, Exception ex)
-        {
-            Console.WriteLine("Critical : " + eventName + " : Exception : " + ex.Message);
-        }
-
-        public void Critical(string eventName, string message, Exception ex)
-        {
-            Console.WriteLine("Critical : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Critical(string eventName, string message, Exception ex, params object[] args)
-        {
-            Console.WriteLine("Critical : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Error(string eventName)
-        {
-            Console.WriteLine("Error : " + eventName);
-        }
-
-        public void Error(string eventName, string message)
-        {
-            Console.WriteLine("Error : " + eventName + " : " + message);
-        }
-
-        public void Error(string eventName, Exception ex)
-        {
-            Console.WriteLine("Error : " + eventName + " : Exception : " + ex.Message);
-        }
-
-        public void Error(string eventName, string message, Exception ex)
-        {
-            Console.WriteLine("Error : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Error(string eventName, string message, Exception ex, params object[] args)
-        {
-            Console.WriteLine("Error : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Informational(string eventName)
-        {
-            Console.WriteLine("Info : " + eventName);
-        }
-
-        public void Informational(string eventName, string message)
-        {
-            Console.WriteLine("Info : " + eventName + " : " + message);
-        }
-
-        public void Informational(string eventName, Exception ex)
-        {
-            Console.WriteLine("Info : " + eventName + " : Exception : " + ex.Message);
-        }
-
-        public void Informational(string eventName, string message, Exception ex)
-        {
-            Console.WriteLine("Info : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Informational(string eventName, string message, Exception ex, params object[] args)
-        {
-            Console.WriteLine("Info : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Verbose(string eventName)
-        {
-            Console.WriteLine("Verbose : " + eventName);
-        }
-
-        public void Verbose(string eventName, string message)
-        {
-            Console.WriteLine("Verbose : " + eventName + " : " + message);
-        }
-
-        public void Verbose(string eventName, Exception ex)
-        {
-            Console.WriteLine("Verbose : " + eventName + " : Exception : " + ex.Message);
-        }
-
-        public void Verbose(string eventName, string message, Exception ex)
-        {
-            Console.WriteLine("Verbose : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Verbose(string eventName, string message, Exception ex, params object[] args)
-        {
-            Console.WriteLine("Verbose : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Warning(string eventName)
-        {
-            Console.WriteLine("Warning : " + eventName);
-        }
-
-        public void Warning(string eventName, string message)
-        {
-            Console.WriteLine("Warning : " + eventName + " : " + message);
-        }
-
-        public void Warning(string eventName, Exception ex)
-        {
-            Console.WriteLine("Warning : " + eventName + " : Exception : " + ex.Message);
-        }
-
-        public void Warning(string eventName, string message, Exception ex)
-        {
-            Console.WriteLine("Warning : " + eventName + " : " + message + " : Exception : " + ex.Message);
-        }
-
-        public void Warning(string eventName, string message, Exception ex, params object[] args)
-        {
-            Console.WriteLine("Warning : " + eventName + " : " + message + " : Exception : " + ex.Message);
+            Environment.Exit(exitCode);
         }
     }
 }
